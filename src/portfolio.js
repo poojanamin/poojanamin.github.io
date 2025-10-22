bf9f45dfbfc87ae99b03f864c1a2c5071d852110
poojanamin <poojanamin0408@gmail.com>
2025-10-22 10:19:59 -0400

    Fix Greeting

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,7 +21,7 @@ const illustration = {
 
 const greeting = {
   username: "Pooja Amin",
-  title: "Hi all, I'm Pooja",
+  title: "Hi! I'm Pooja",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with Angular / JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
